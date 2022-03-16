756948ade7524e277ce9b1754607e402fbd33016
rameez <codewithrameez@gmail.com>
2022-03-17 00:43:45 +0530

    Created Main Activity, Wired 2 Input Activities to it

--- app/src/main/java/com/mc/josaahelper/MainActivity.java
+++ app/src/main/java/com/mc/josaahelper/MainActivity.java
@@ -2,13 +2,37 @@ package com.mc.josaahelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mRankButton, mCollegeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRankButton = findViewById(R.id.button);
+        mCollegeButton = findViewById(R.id.button2);
+
+        mRankButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RankInputsActivity.class);
+                //i.putExtra("username", mUsername);
+                startActivity(i);
+            }
+        });
+
+        mCollegeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CollegeInputsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/mc/josaahelper/RankOutputsActivity.java
@@ -0,0 +1,14 @@
+package com.mc.josaahelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RankOutputsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank_outputs);
+    }
+}
