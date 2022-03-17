b5e3f203dc6d3a122e524a2acdc63959a094a68b
rameez <codewithrameez@gmail.com>
2022-03-18 02:06:47 +0530

    Added input functionality to Rank Predictor

--- app/src/main/java/com/mc/josaahelper/RankInputsActivity.java
+++ app/src/main/java/com/mc/josaahelper/RankInputsActivity.java
@@ -2,20 +2,73 @@ package com.mc.josaahelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class RankInputsActivity extends AppCompatActivity {
+    private RadioGroup mExamRadioGroup;
+    private RadioButton mExamRadioButton;
+    private Spinner mCategorySpinner;
+    private EditText mMarksEditText;
+    private Button mPredictButton;
+    private String mExamType, mCategory;
+    private int mMarks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank_inputs);
 
-        Spinner spinnerCategories = findViewById(R.id.spinner);
+        mCategorySpinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spinnerCategories.setAdapter(adapter);
+        mCategorySpinner.setAdapter(adapter);
+
+        mExamRadioGroup = findViewById(R.id.radioGroup);
+        mMarksEditText = findViewById(R.id.editTextNumber);
+        mPredictButton = findViewById(R.id.button3);
+
+        mPredictButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mExamRadioGroup.getCheckedRadioButtonId() == -1){
+                    Toast.makeText(RankInputsActivity.this, "Choose an Exam Type", Toast.LENGTH_SHORT).show();
+                }
+                else if(mCategorySpinner.getSelectedItemId() == -1){
+                    Toast.makeText(RankInputsActivity.this, "Choose a Category", Toast.LENGTH_SHORT).show();
+                }
+                else if (mMarksEditText.getText().toString().trim().length() == 0){
+                    Toast.makeText(RankInputsActivity.this, "Enter Marks", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mExamRadioButton = (RadioButton) findViewById(mExamRadioGroup.getCheckedRadioButtonId());
+                    mExamType = mExamRadioButton.getText().toString();
+                    mCategory = mCategorySpinner.getSelectedItem().toString();
+                    mMarks = Integer.parseInt(mMarksEditText.getText().toString());
+
+                    if (mExamRadioGroup.getCheckedRadioButtonId() == R.id.radioButton && (mMarks < 0 || mMarks > 300)){
+                        Toast.makeText(RankInputsActivity.this, "For Jee Mains: Enter marks in range 0 to 300", Toast.LENGTH_SHORT).show();
+                    }
+                    else if (mExamRadioGroup.getCheckedRadioButtonId() == R.id.radioButton2 && (mMarks < 0 || mMarks > 366)){
+                        Toast.makeText(RankInputsActivity.this, "For Jee Advanced: Enter marks in range 0 to 366", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Intent i = new Intent(RankInputsActivity.this, RankOutputsActivity.class);
+                        i.putExtra("examtype", mExamType);
+                        i.putExtra("category", mCategory);
+                        i.putExtra("marks", mMarks);
+                        startActivity(i);
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/mc/josaahelper/RankOutputsActivity.java
+++ app/src/main/java/com/mc/josaahelper/RankOutputsActivity.java
@@ -3,12 +3,29 @@ package com.mc.josaahelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RankOutputsActivity extends AppCompatActivity {
+    private String mExamType, mCategory;
+    private int mMarks;
+    private TextView mExamTV, mCategoryTV, mMarksTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank_outputs);
+
+        Bundle extras = getIntent().getExtras();
+        mExamType = extras.getString("examtype");
+        mCategory = extras.getString("category");
+        mMarks = extras.getInt("marks");
+
+        mExamTV = findViewById(R.id.textView11);
+        mCategoryTV = findViewById(R.id.textView12);
+        mMarksTV = findViewById(R.id.textView13);
+
+        mExamTV.setText(mExamType);
+        mCategoryTV.setText(mCategory);
+        mMarksTV.setText(String.valueOf(mMarks));
     }
 }
