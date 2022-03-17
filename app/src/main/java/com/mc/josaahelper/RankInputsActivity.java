0a66a74635eb03f3c3b1a8e2e3bfc4454c0d830a
rameez <codewithrameez@gmail.com>
2022-03-17 23:13:01 +0530

    Added input fields to Rank Predictor

--- app/src/main/java/com/mc/josaahelper/RankInputsActivity.java
+++ app/src/main/java/com/mc/josaahelper/RankInputsActivity.java
@@ -3,6 +3,8 @@ package com.mc.josaahelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RankInputsActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RankInputsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank_inputs);
+
+        Spinner spinnerCategories = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerCategories.setAdapter(adapter);
     }
 }
