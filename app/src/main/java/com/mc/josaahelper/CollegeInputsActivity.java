418df50212750750471563b4bd1a2283936de2d4
rameez03 <84805319+rameez03@users.noreply.github.com>
2022-03-21 23:03:38 +0530

    Revert "Merge pull request #3 from rameez03/v0.0.1"
    This reverts commit c1015817004be7c57cfca9bcb296f2574e179258, reversing
    changes made to f10a95abb968c2369282717810d36e880e3f8bef.

--- app/src/main/java/com/mc/josaahelper/CollegeInputsActivity.java
+++ app/src/main/java/com/mc/josaahelper/CollegeInputsActivity.java
@@ -2,76 +2,13 @@ package com.mc.josaahelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class CollegeInputsActivity extends AppCompatActivity {
-    private RadioGroup mExamRadioGroup;
-    private RadioButton mExamRadioButton;
-    private Spinner mCategorySpinner;
-    private EditText mMarksEditText;
-    private Button mNextButton;
-    private String mExamType, mCategory;
-    private int mMarks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_college_inputs);
-
-        mCategorySpinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        mCategorySpinner.setAdapter(adapter);
-
-        mExamRadioGroup = findViewById(R.id.radioGroup);
-        mMarksEditText = findViewById(R.id.editTextNumber);
-        mNextButton = findViewById(R.id.button3);
-
-        mNextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mExamRadioGroup.getCheckedRadioButtonId() == -1){
-                    Toast.makeText(CollegeInputsActivity.this, "Choose an Exam Type", Toast.LENGTH_SHORT).show();
-                }
-                else if(mCategorySpinner.getSelectedItemId() == -1){
-                    Toast.makeText(CollegeInputsActivity.this, "Choose a Category", Toast.LENGTH_SHORT).show();
-                }
-                else if (mMarksEditText.getText().toString().trim().length() == 0){
-                    Toast.makeText(CollegeInputsActivity.this, "Enter Marks", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    mExamRadioButton = (RadioButton) findViewById(mExamRadioGroup.getCheckedRadioButtonId());
-                    mExamType = mExamRadioButton.getText().toString();
-                    mCategory = mCategorySpinner.getSelectedItem().toString();
-                    mMarks = Integer.parseInt(mMarksEditText.getText().toString());
-
-                    if (mExamRadioGroup.getCheckedRadioButtonId() == R.id.radioButton && (mMarks < 0 || mMarks > 300)){
-                        Toast.makeText(CollegeInputsActivity.this, "For Jee Mains: Enter marks in range 0 to 300", Toast.LENGTH_SHORT).show();
-                    }
-                    else if (mExamRadioGroup.getCheckedRadioButtonId() == R.id.radioButton2 && (mMarks < 0 || mMarks > 366)){
-                        Toast.makeText(CollegeInputsActivity.this, "For Jee Advanced: Enter marks in range 0 to 366", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-
-                        //Moving to the next activity
-
-                        Intent i = new Intent(CollegeInputsActivity.this, CollegeOutputsActivity.class);
-                        i.putExtra("examtype", mExamType);
-                        i.putExtra("category", mCategory);
-                        i.putExtra("marks", mMarks);
-                        startActivity(i);
-                    }
-                }
-            }
-        });
     }
 }
--- app/src/main/java/com/mc/josaahelper/CollegeOutputsActivity.java
+++ app/src/main/java/com/mc/josaahelper/CollegeOutputsActivity.java
@@ -2,77 +2,13 @@ package com.mc.josaahelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class CollegeOutputsActivity extends AppCompatActivity {
 
-    private Spinner mBranchSpinner;
-    private Spinner mQuotaSpinner;
-    private Spinner mGenderSpinner;
-    private Button mPredict;
-    private String mBranch;
-    private String mQuota;
-    private String mGender;
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_college_outputs);
-
-        mBranchSpinner = findViewById(R.id.spinner);
-        mQuotaSpinner = findViewById(R.id.spinner2);
-        mGenderSpinner = findViewById(R.id.spinner3);
-        mPredict = findViewById(R.id.button3);
-
-        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this, R.array.branch, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        mBranchSpinner.setAdapter(adapter);
-
-        ArrayAdapter<CharSequence> adapter1 =ArrayAdapter.createFromResource(this, R.array.quota, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        mQuotaSpinner.setAdapter(adapter1);
-
-        ArrayAdapter<CharSequence> adapter2 =ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        mGenderSpinner.setAdapter(adapter2);
-
-        mPredict.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mBranchSpinner.getSelectedItemId() == -1){
-                    Toast.makeText(CollegeOutputsActivity.this, "Choose a branch", Toast.LENGTH_SHORT).show();
-                }
-                else if(mQuotaSpinner.getSelectedItemId() == -1){
-                    Toast.makeText(CollegeOutputsActivity.this, "Choose a Quota", Toast.LENGTH_SHORT).show();
-                }
-                else if (mGenderSpinner.getSelectedItemId() == -1){
-                    Toast.makeText(CollegeOutputsActivity.this, "Choose a gender", Toast.LENGTH_SHORT).show();
-                }
-                else {
-
-                    mBranch = mBranchSpinner.getSelectedItem().toString();
-                    mQuota = mQuotaSpinner.getSelectedItem().toString();
-                    mGender = mGenderSpinner.getSelectedItem().toString();
-
-                    //Moving to the next activity
-                    Intent i = new Intent(CollegeOutputsActivity.this, CollegeOutputsActivityFinal.class);
-                    i.putExtra("examtype", getIntent().getStringExtra("examtype"));
-                    i.putExtra("category", getIntent().getStringExtra("category"));
-                    i.putExtra("marks", getIntent().getStringExtra("marks"));
-                    i.putExtra("branch", mBranch);
-                    i.putExtra("quota", mQuota);
-                    i.putExtra("gender", mGender);
-
-                    startActivity(i);
-
-                }
-            }
-        });
     }
 }
