8845e292a95f7c5aed5ee25ad8d309ef9c94505b
rameez03 <84805319+rameez03@users.noreply.github.com>
2022-05-09 01:45:23 +0530

    Database Connectivity established.

--- app/src/main/java/com/mc/josaahelper/CollegeProfileActivity.java
+++ app/src/main/java/com/mc/josaahelper/CollegeProfileActivity.java
@@ -10,24 +10,18 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class CollegeProfileActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
-    private FirebaseFirestore db;
     private CollegeProfileAdapter mCollegeProfileAdapter;
+    private FirebaseFirestore db;
     private List<CollegeModel> mCollegeList;
 
     @Override
@@ -36,79 +30,35 @@ public class CollegeProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_college_profile);
 
         mRecyclerView = findViewById(R.id.profileRecylerView);
-        mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         db = FirebaseFirestore.getInstance();
         mCollegeList = new ArrayList<>();
+        showData();
         mCollegeProfileAdapter = new CollegeProfileAdapter(this , mCollegeList);
         mRecyclerView.setAdapter(mCollegeProfileAdapter);
         
-        showData();
 
-        //addUser();
-        
-    }
-
-    private void addUser() {
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("CollegeProfileActivity", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("CollegeProfileActivity", "Error adding document", e);
-                    }
-                });
     }
 
     private void showData() {
         db.collection("JOSSA_INSTITUTE")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            mCollegeList.clear();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("CollegeProfileActivity", document.getId() + " => " + document.getData());
-                                CollegeModel model = new CollegeModel(document.getString("sNo"), document.getString("instituteCodeAndName"));
-                                mCollegeList.add(model);
-                            }
-                            mCollegeProfileAdapter.notifyDataSetChanged();
-                        } else {
-                            Toast.makeText(CollegeProfileActivity.this, "Error getting documents.", Toast.LENGTH_SHORT).show();
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        mCollegeList.clear();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Log.d("CollegeProfileActivity", document.getString("sNo") + " => " + document.getString("instituteCodeAndName"));
+                            CollegeModel model = new CollegeModel(document.getString("sNo"), document.getString("instituteCodeAndName"));
+                            mCollegeList.add(model);
                         }
+                        mCollegeProfileAdapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(CollegeProfileActivity.this, "Error getting documents.", Toast.LENGTH_SHORT).show();
                     }
-                });
-//        db.collection("JOSSA_INSTITUTE").get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        mCollegeList.clear();
-//                        for (DocumentSnapshot snapshot: task.getResult()){
-//                            CollegeModel model = new CollegeModel(snapshot.getString("sNo"), snapshot.getString("instituteCodeAndName"));
-//                            mCollegeList.add(model);
-//                        }
-//                        mCollegeProfileAdapter.notifyDataSetChanged();
-//                    }
-//                }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Toast.makeText(CollegeProfileActivity.this, "Something went Wrong!!!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+                }
+            });
     }
 }
--- app/src/main/java/com/mc/josaahelper/CollegeProfileAdapter.java
+++ app/src/main/java/com/mc/josaahelper/CollegeProfileAdapter.java
@@ -14,7 +14,6 @@ public class CollegeProfileAdapter extends RecyclerView.Adapter<CollegeProfileAd
     private CollegeProfileActivity activity;
     private List<CollegeModel> mCollegeList;
 
-
     public CollegeProfileAdapter(CollegeProfileActivity activity , List<CollegeModel> mCollegeList){
         this.activity = activity;
         this.mCollegeList = mCollegeList;
@@ -22,15 +21,15 @@ public class CollegeProfileAdapter extends RecyclerView.Adapter<CollegeProfileAd
 
     @NonNull
     @Override
-    public CollegeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CollegeProfileAdapter.CollegeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(activity).inflate(R.layout.collegelistitem , parent , false);
         return new CollegeViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CollegeViewHolder holder, int position) {
-        holder.instituteIDTV.setText(mCollegeList.get(position).getInstituteID());
-        holder.instituteNameTV.setText(mCollegeList.get(position).getInstituteName());
+        holder.instituteIDTV.setText(mCollegeList.get(holder.getAdapterPosition()).getInstituteID());
+        holder.instituteNameTV.setText(mCollegeList.get(holder.getAdapterPosition()).getInstituteName());
     }
 
     @Override
